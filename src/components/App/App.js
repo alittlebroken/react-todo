eea7354bfc5223a62ee65a33bced5a4928b9446b
Paul Lockyer <plockyer@googlemail.com>
2021-09-23 11:28:06 +0100

    Imported base componetns and set up dummy task data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+// Import custom compenents
+import TaskBar from '../TaskBar/TaskBar';
+import TaskList from '../TaskList/TaskList';
 
 function App() {
+
+  // Setup state for Tasks and Task List
+  const [task, setTask] = useState({});
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      name: 'Create a react todo app'
+    },
+    {
+      id: 2,
+      name: 'Take out the trash'
+    },
+    {
+      id: 3,
+      name: 'Pick Son up from nursery'
+    },
+    {
+      id: 4,
+      name: 'Find a new place to live'
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskBar />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
