16bbd26ec390dd8d35c93fc835f5732db4d3b6df
Paul Lockyer <plockyer@googlemail.com>
2021-09-23 11:27:26 +0100

    Created base TaskBar component

---
+++ src/components/TaskBar/TaskBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './TaskBar.css';
+
+function TaskBar(){
+
+  return (
+    <div>
+      <form>
+        <input />
+        <button>Add Task</button>
+      </form>
+    </div>
+  );
+
+}
+
+export default TaskBar;
