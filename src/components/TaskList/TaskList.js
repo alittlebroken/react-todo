bb0d9d5f1d16472f007a4dc649922d801ee277a3
Paul Lockyer <plockyer@googlemail.com>
2021-09-23 11:27:09 +0100

    Created base TaskList component

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './TaskList.css';
+
+// Custom component Imports
+import Task from '../Task/Task';
+
+// Main functional component
+function TaskList(props){
+
+  // Extract task list from props
+  const taskList = props.tasks;
+
+  return (
+    <div>
+      <h2>Current Task List</h2>
+
+      {taskList.map((task) => {
+        return (
+          <Task task={task} />
+        );
+      })}
+    </div>
+  )
+};
+
+// Exports
+export default TaskList;
